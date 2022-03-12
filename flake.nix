2c6461a63a89c9c4ac2e8b256fc649e055471296
hugosenari <hugosenari@gmail.com>
2022-03-12 02:10:11 -0300

    chore: re index hayaa

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = ''Conway's Game of Life implemented in Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."hayaa-main".url = "path:./main";
+  inputs."hayaa-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."hayaa-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."hayaa-v0_2_0".url = "path:./v0_2_0";
+  inputs."hayaa-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."hayaa-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."hayaa-v0_2_1".url = "path:./v0_2_1";
+  inputs."hayaa-v0_2_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."hayaa-v0_2_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."hayaa-v0_3_0".url = "path:./v0_3_0";
+  inputs."hayaa-v0_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."hayaa-v0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v0_2_1/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Conway's Game of Life implemented in Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-hayaa-v0_2_1.flake = false;
+  inputs.src-hayaa-v0_2_1.owner = "angus-lherrou";
+  inputs.src-hayaa-v0_2_1.ref   = "refs/tags/v0.2.1";
+  inputs.src-hayaa-v0_2_1.repo  = "hayaa";
+  inputs.src-hayaa-v0_2_1.type  = "github";
+  
+  inputs."illwill".dir   = "nimpkgs/i/illwill";
+  inputs."illwill".owner = "riinr";
+  inputs."illwill".ref   = "flake-pinning";
+  inputs."illwill".repo  = "flake-nimble";
+  inputs."illwill".type  = "github";
+  inputs."illwill".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."illwill".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-hayaa-v0_2_1"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-hayaa-v0_2_1";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
